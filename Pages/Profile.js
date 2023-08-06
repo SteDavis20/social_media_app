2fc38c8f4645367a13547d0add04c7485aa07e72
Stephen Davis <stephendavis2016@gmail.com>
2023-08-06 18:03:11 +0100

    Adding dummy data to Profile page

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,10 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function Profile() {
   return (
     <View style={styles.container}>
       <Text>Profile page!</Text>
+      {/* Profile picture */}
+      <Image
+        source={require("../assets/icon.png")}
+        style={styles.profilePicture}
+      />
+      {/* Username */}
+      <Text>@Joe_Bloggs</Text>
+
+      <View style={{ flexDirection: "row" }}>
+        {/* Number of posts */}
+        <Text style={styles.statistic}>18 posts</Text>
+
+        {/* Number of followers */}
+        <Text style={styles.statistic}>100 followers</Text>
+
+        {/* Number of following */}
+        <Text style={styles.statistic}>110 following</Text>
+      </View>
+
+      {/* Name */}
+      <Text style={{}}>Joe Bloggs</Text>
+
+      {/* Bio */}
+      <Text style={{}}>Irish, TCD '27 CSB Graduate, Software Engineer</Text>
+
+      {/* Highlights */}
+
+      {/* Display posts */}
+      {/* Edit profile */}
+      {/* Logout */}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +47,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  profilePicture: { width: 100, height: 100 },
+  statistic: { padding: 10 },
+  username: {},
+  name: {},
+  bio: {},
 });
